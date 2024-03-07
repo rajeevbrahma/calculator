43b52d94bfcdb2c44e7fb6681ea1dc046dc3461e
beulah4 <beulahankam@gmail.com>
2024-03-07 16:19:20 -0500

    modular division added

--- calc_main.py
+++ calc_main.py
@@ -1,15 +1,3 @@
-def add(a,b):
-    return (a+b)
-
-def sub(a,b):
-    if (a>b):
-        return (a-b)
-    return (b-a)
-
-def div(a,b):
-    return (a/b)
-
-def mul(a,b):
-    return (a*b)
-
-print (add(3,4))
+l = [x for x in range(10)
+     if x % 2 == 0]
+print(l)
